f3cecc74052be4446ccdd87b27fcf2abb7dfbaba
Abed Rezaey <ar@abedrezaey.com>
2022-04-19 23:26:10 -0400

    Assignment D2

---
+++ w1/d1/script.js
@@ -0,0 +1,32 @@
+//comments 
+//comments
+//comments
+//comments
+//console.log
+
+
+//variables 
+
+var x = 25
+console.log(x)
+
+
+var myname = 'abed'
+console.log(myname);
+
+
+
+
+//loops
+
+for (var i = 0;i < 10; i++){
+      console.log
+}
+
+var myarr=
+[1,2,'x',x];
+
+console.log(myarr[2])
+
+for(var i = 0 i< myarr.length;)
+

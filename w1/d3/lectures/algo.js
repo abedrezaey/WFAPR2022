94fc9c327d36a5b584a9bc3924ac1d7b173002ec
Abed Rezaey <ar@abedrezaey.com>
2022-04-21 23:11:54 -0400

    assignment d4

---
+++ w1/d3/lectures/algo.js
@@ -0,0 +1,50 @@
+var isSunny = true;
+
+var temperature = 45; // let's assume degrees Fahrenheit
+var isRaining = false;
+var whatToBring = "I should bring: ";
+    
+if(isSunny) {
+  whatToBring += "sunglasses, ";
+}
+if(temperature < 50) {
+  whatToBring += "a coat, ";
+}
+if(isRaining) {
+  whatToBring += "an umbrella, ";
+}
+whatToBring += "and a smile!";
+    
+console.log(whatToBring);
+
+
+
+
+//Prepare for downcount
+for(var i=10; i>0; i--) {
+  if(i != 2) {
+    console.log(i);
+  } else {
+    console.log("ignition!");
+  }
+}
+    
+console.log("liftoff!");
+
+
+
+
+
+//Count positives
+
+var countPositives = 0;
+var numbers = [3, 4, -2, 7, 16, -8, 0];
+
+// your code here!
+    
+console.log("there are " + countPositives + " positive values");
+
+
+
+
+

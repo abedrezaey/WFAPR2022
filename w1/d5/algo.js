f4e47c6dc92afe6e7849536362da02e4bd536db1
Abed Rezaey <ar@abedrezaey.com>
2022-04-25 01:20:03 -0400

    D4 Assignment

---
+++ w1/d5/algo.js
@@ -0,0 +1,47 @@
+var fruit1 = "apples";
+var fruit2 = "oranges";
+    
+var temp = fruit1; // temp is a common name for this
+fruit1 = fruit2;
+fruit2 = temp;
+    
+console.log(fruit2 + " and " + fruit1);
+
+
+
+
+
+var start = 0;
+var end = 12;
+    
+
+while(start < end) {
+    console.log("start: " + start + ", end: " + end);
+    start += 2;
+    end -= 2;
+}
+
+
+
+
+
+
+// Reversing an array
+// Write a function reverse( arr ) to reverse an array, if we were given...
+
+
+reverse (arrary1) ["a", "b", "c", "d", "e"];
+
+function reverse(arr){
+      for (var i = 0; <= (arr.lengh / 2) ; i++){
+
+      }
+}
+
+
+
+
+
+// we expect to get back...
+
+["e", "d", "c", "b", "a"];
---
+++ w1/d5/lecure/script.js
@@ -0,0 +1,31 @@
+if(mynum == 'four'){
+      console.log
+}
+
+
+
+
+//loops
+
+
+for(var i =0; i<10;i++)
+console.log(i)
+
+
+
+var i =0
+while(i<10){
+      console.log(i)
+      i+=1
+    
+
+      
+      
+// declare
+      function setx (value, value3){
+            x = value
+      }
+
+
+//cal
+      setx(4)

0ab7cd5d915361a1f8a79b8fa5467c21f4b23c61
Abed Rezaey <ar@abedrezaey.com>
2022-04-27 13:13:34 -0400

    JSAssignmentPart2Update

---
+++ W2/D1/assignment/LikingLikesPart2/script.js
@@ -0,0 +1,35 @@
+var likes = 0;
+var likesElement = document.querySelector("#likes");
+console.log(likesElement);
+function add1() {
+      likes++;
+      likesElement.innerText = "" + likes;
+      console.log(likes);
+      
+}
+
+
+
+var likes01 = 0;
+var likes01Element = document.querySelector("#likes01");
+console.log(likes01Element);
+function add1b() {
+      likes01++;
+      likes01Element.innerText = "" + likes01;
+      console.log(likes01);
+      
+}
+
+
+
+var likes02 = 0;
+var likes02Element = document.querySelector("#likes02");
+console.log(likes02Element);
+function add1c() {
+      likes02++;
+      likes02Element.innerText = "" + likes02;
+      console.log(likes02);
+      
+}
+
+

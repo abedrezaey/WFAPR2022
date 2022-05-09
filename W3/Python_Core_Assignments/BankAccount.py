c9145a011bd39ebd6806e03189ad6870977cfd56
Abed Rezaey <ar@abedrezaey.com>
2022-05-08 23:17:53 -0400

    Updates

--- W3/Python_Core_Assignments/BankAccount.py
+++ W3/Python_Core_Assignments/BankAccount.py
@@ -1,7 +1,9 @@
 class BankAccount:
       # don't forget to add some default values for these parameters!
       def __init__(self, int_rate, balance): 
+
             
+
             # your code here! (remember, instance attributes go here)
             # don't worry about user info here; we'll involve the User class soon
       def deposit(self, amount):
